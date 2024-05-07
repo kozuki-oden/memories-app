e3eca190f6d937f047c6be2766d84fac05a1337c
Aditya Sood <aditya12sc@gmail.com>
2024-05-07 22:36:19 +0530

    Update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -43,7 +43,7 @@ const Form = ({ currentId, setCurrentId }) => {
     return (
       <Paper className={classes.paper} elevation={6}>
         <Typography variant="h6" align="center">
-          Please Sign In to create your own memories and like other's memories.
+          Please sign-in to create your own stories and like other's stories.
         </Typography>
       </Paper>
     );
